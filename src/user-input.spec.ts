f8f602b594845b2876284b8869c65a75e218f106
Jan Wendland <jan.we@exodus.io>
2023-03-13 10:44:37 +0100

    feat: add util to normalize user supplied package names

---
+++ src/user-input.spec.ts
@@ -0,0 +1,111 @@
+import { Volume } from 'memfs/lib/volume'
+import { normalizePackages } from './user-input'
+
+describe('normalizePackages', () => {
+  let fs: Volume
+
+  const packageContents = {
+    storageMobile: JSON.stringify({
+      name: '@exodus/storage-mobile',
+    }),
+    config: JSON.stringify({
+      name: '@exodus/config',
+    }),
+    formatting: JSON.stringify({
+      name: '@exodus/formatting',
+    }),
+    theUltimatePackage: JSON.stringify({
+      name: '@exodus/the-ultimate-package',
+    }),
+  }
+  const lernaConfig = JSON.stringify({
+    packages: ['libraries/*', 'modules/*', 'deeply/nested/package/root/*'],
+    version: 'independent',
+    npmClient: 'yarn',
+    useWorkspaces: true,
+    useNx: true,
+  })
+
+  beforeEach(() => {
+    fs = Volume.fromJSON({
+      'lerna.json': lernaConfig,
+      'modules/storage-mobile/package.json': packageContents.storageMobile,
+      'deeply/nested/package/root/the-ultimate-package/package.json':
+        packageContents.theUltimatePackage,
+      'modules/config/package.json': packageContents.config,
+      'libraries/formatting/package.json': packageContents.formatting,
+    })
+  })
+
+  it('should trim spaces', async () => {
+    const result = await normalizePackages({
+      packagesCsv: ' libraries/formatting, modules/config ',
+      filesystem: fs as never,
+    })
+    expect(result).toEqual(['libraries/formatting', 'modules/config'])
+  })
+
+  it('should remove empty values', async () => {
+    const result = await normalizePackages({
+      packagesCsv: ',libraries/formatting, ,modules/config,',
+      filesystem: fs as never,
+    })
+    expect(result).toEqual(['libraries/formatting', 'modules/config'])
+  })
+
+  it('should normalize paths', async () => {
+    const result = await normalizePackages({
+      packagesCsv: 'formatting,config',
+      filesystem: fs as never,
+    })
+    expect(result).toEqual(['libraries/formatting', 'modules/config'])
+  })
+
+  it('should normalize package names', async () => {
+    const result = await normalizePackages({
+      packagesCsv: '@exodus/formatting, @exodus/config ',
+      filesystem: fs as never,
+    })
+    expect(result).toEqual(['libraries/formatting', 'modules/config'])
+  })
+
+  it('should normalize deeply nested package name', async () => {
+    const result = await normalizePackages({
+      packagesCsv: '@exodus/the-ultimate-package',
+      filesystem: fs as never,
+    })
+    expect(result).toEqual(['deeply/nested/package/root/the-ultimate-package'])
+  })
+
+  it('should normalize deeply nested short name', async () => {
+    const result = await normalizePackages({
+      packagesCsv: 'the-ultimate-package',
+      filesystem: fs as never,
+    })
+    expect(result).toEqual(['deeply/nested/package/root/the-ultimate-package'])
+  })
+
+  it('should throw for non existing short names', async () => {
+    const promise = normalizePackages({
+      packagesCsv: 'batcave, formatting',
+      filesystem: fs as never,
+    })
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('should throw for non existing package names', async () => {
+    const promise = normalizePackages({
+      packagesCsv: '@exodus/formatting,@exodus/wayne-enterprises,@exodus/batcave',
+      filesystem: fs as never,
+    })
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('should throw for non existing package roots', async () => {
+    const promise = normalizePackages({
+      packagesCsv: 'packages/formatting,misc/test',
+      filesystem: fs as never,
+    })
+    await expect(promise).rejects.toThrow()
+  })
+})
