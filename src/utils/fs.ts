a0fe0839cc92c028b3f0dca12958579b8ad20500
Jan W <jan.we@exodus.io>
2024-03-15 19:53:12 +0100

    fix: harden readJson

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,15 +1,26 @@
 import * as fs from 'fs'
+import * as path from 'path'
+import * as assert from 'assert'
 
 export async function readJson<ReturnType>(
-  path: string,
+  filePath: string,
   { filesystem }: { filesystem: typeof fs }
 ): Promise<ReturnType | undefined> {
-  let buffer: Buffer
+  assert(
+    path.extname(filePath) === '.json',
+    `Can only parse .json files. Received ${path.basename(filePath)}`
+  )
+
+  let content: string
   try {
-    buffer = await filesystem.promises.readFile(path)
+    content = await filesystem.promises.readFile(filePath, 'utf8')
   } catch {
     return undefined
   }
 
-  return JSON.parse(buffer.toString())
+  try {
+    return JSON.parse(content)
+  } catch {
+    throw new Error(`Failed to parse ${filePath}`)
+  }
 }
